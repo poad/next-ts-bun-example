b1fba491085d6ff416ed8811dfb3c4dd592c8bbe
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-10 13:08:28 +0900

    Bumps node modules

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import nextPlugin from '@next/eslint-plugin-next';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: [
+      '**/*.d.ts',
+      '*.js',
+      'src/tsconfig.json',
+      'src/next-env.d.ts',
+      'src/stories',
+      'node_modules/**/*',
+    ],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@next/next/no-duplicate-head': 'off',
+      '@next/next/no-img-element': 'error',
+    },
+  },
+  {
+    ignores: ['./.next/*'],
+  },
+];
